f85dd1192414d17acb56635b0156ece768bd7786
Quang Khải <khai151020@gmail.com>
2022-05-24 08:34:06 +0700

    change to commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors({
 }));
 
 app.use(morgan('combined'))
-
+console.log("HELLO");
 app.use(express.json());  
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/v1', api_v1);// similar to next api version
